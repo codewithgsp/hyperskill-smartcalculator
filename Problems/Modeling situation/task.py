150e8149c0077fbd061a0ada9608d753e3c9764f
codewithgsp <codewithgsp@gmail.com>
2020-07-02 11:37:04 +0200

    modeling situation

---
+++ Problems/Modeling situation/task.py
@@ -0,0 +1,11 @@
+try:
+    exception_test()
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+except ArithmeticError:
+    print('ArithmeticError')
+except AssertionError:
+    print('AssertionError')
+
+
+
